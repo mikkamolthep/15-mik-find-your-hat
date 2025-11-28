0af9b53e0880bc7464e5de5f4aedcce4e0119e82
mikkamolthep <mik.kamolthep@gmail.com>
2025-11-28 19:55:15 +0700

    15-find-your-hat

--- main.js
+++ main.js
@@ -24,10 +24,70 @@ let playing = true;
 // Print board
 function printBoard(board) {
 	console.clear(); // call console.clear() before print each move
-	console.log(board);
+	board.forEach(walk => console.log(walk.join(" ")));
+  
 }
 
+
+
 // Game play loop
+while(playing){
 printBoard(board);
-const input = prompt("Which way? (w/a/s/d): ");
-console.log(input);
+
+const input = prompt("Which way? (w/a/s/d): ".toLowerCase());
+
+board[playerRow][playerCol] = EMPTY;
+
+movement(input);
+playing = status()
+
+board[playerRow][playerCol] = PLAYER;
+console.log("End");
+}
+
+
+function movement (input) {
+  switch (input) {
+    case "w" :
+      playerRow--;
+      return true;
+      
+    case "a" :
+      playerCol--;
+      return true;
+    case "s" :
+      playerRow++;
+      return true;
+    case "d" :
+      playerCol++;
+      return true;
+    default :
+      
+    return false;
+  }
+};
+
+
+function status (){
+
+  if(playerRow < 0 || playerRow >= board.length || playerCol < 0 || playerCol >= board.length){
+    console.log("You walk out of board.Try again")
+    return false;
+  }
+
+  const moves = board[playerRow][playerCol];
+
+  if(moves === HOLE) {
+    console.log("You fell into the hole.Try again");
+    return false;
+  }
+  else if (moves === HAT) {
+    console.log("You found the hat");
+    return false;
+  }
+  else {
+    return true;
+  }
+
+  
+}
